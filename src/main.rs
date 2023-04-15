18087278ec35af784a4f4deeaa24d138a643dacb
andreasbaumgartner <baumgartner.andy@hotmail.ch>
2023-04-15 18:54:43 +0200

    add code to search for a requirements file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let global = check_requirements_file_exist();
+    println!("Global: {:?}", global);
+}
+
+use glob::glob;
+
+fn check_requirements_file_exist() {
+    for e in glob("*/*").expect("Failed to read glob pattern") {
+        let found = e;
+        // check if the file name is requirements.txt
+        if found
+            .unwrap()
+            .display()
+            .to_string()
+            .contains("requirements.txt")
+        {
+            println!("Found a file named requirements.txt",);
+        }
+    }
 }
