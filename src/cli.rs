484ed10886cacf562afc5d28f2003ae6f67c363d
andreasbaumgartner <baumgartner.andy@hotmail.ch>
2023-04-16 21:34:43 +0200

    rename command to search pattern

--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 struct SearchPackages {
     /// The pattern to look for
-    command: String,
+    pattern: String,
     /// The path to the file to read
     path: std::path::PathBuf,
 }
@@ -15,7 +15,7 @@ pub fn search_for_packages() {
         std::fs::read_to_string(args.path).expect("Something went wrong reading the file");
 
     for line in contents.lines() {
-        if line.contains(&args.command) {
+        if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
