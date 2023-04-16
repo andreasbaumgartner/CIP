24320e4748df54c80d8cda69d1d58869ee5903ef
andreasbaumgartner <baumgartner.andy@hotmail.ch>
2023-04-16 15:47:24 +0200

    add looking for folder requirements

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 fn main() {
-    let global = check_requirements_file_exist();
-    println!("Global: {:?}", global);
+    let file = check_requirements_file_exist();
+    println!("Global: {:?}", file);
+
+    let folder = does_folder_foo_exist_in_current_directory();
+    println!("Global: {:?}", folder);
 }
 
 use glob::glob;
 
 fn check_requirements_file_exist() {
-    for e in glob("*/*").expect("Failed to read glob pattern") {
-        let found = e;
-        // check if the file name is requirements.txt
-        if found
-            .unwrap()
-            .display()
-            .to_string()
-            .contains("requirements.txt")
-        {
-            println!("Found a file named requirements.txt",);
+    for e in glob("*/*").expect("Failed to search for requirements file") {
+        let found = e.unwrap();
+        if found.display().to_string().contains("requirements.txt") {
+            println!("Found a file named requirements.txt, {}", found.display());
+        }
+    }
+}
+
+fn does_folder_foo_exist_in_current_directory() {
+    for e in glob("*/*").expect("Failed to search for requirements folder") {
+        let found = e.unwrap();
+        println!("Found: {:?}", found);
+        if found.display().to_string().contains("requirements") && found.is_dir() {
+            println!("Found a folder named requirements, {}", found.display());
         }
     }
 }
